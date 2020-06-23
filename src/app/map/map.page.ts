62a61676a98ab25195b986fd6d7e105da6bc03dd
diogoantonio77 <diogoantonio47@hotmail.com>
2020-06-22 23:05:16 -0300

    geolocalização

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import {ToastController,Platform,LoadingController} from '@ionic/angular';
+import {GoogleMaps,GoogleMap,GoogleMapsEvent,Marker,GoogleMapsAnimation,MyLocation} from '@ionic-native/google-maps';
+import {Polygon,BaseArrayClass,ILatLng,LatLng} from '@ionic-native/google-maps';
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
@@ -7,9 +10,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
-  constructor() { }
+  map: GoogleMap;
+  loading: any;
+  
+  GORYOKAKU_POINTS: ILatLng[] = [
+    {lat: 41.79883,           lng: 140.75675},
+    {lat: 41.799240000000005, lng: 140.75875000000002},
+    {lat: 41.797650000000004, lng: 140.75905},
+    {lat: 41.79637,           lng: 140.76018000000002},
+    {lat: 41.79567,           lng: 140.75845},
+    {lat: 41.794470000000004, lng: 140.75714000000002},
+    {lat: 41.795010000000005, lng: 140.75611},
+    {lat: 41.79477000000001,  lng: 140.75484},
+    {lat: 41.79576,           lng: 140.75475},
+    {lat: 41.796150000000004, lng: 140.75364000000002},
+    {lat: 41.79744,           lng: 140.75454000000002},
+    {lat: 41.79909000000001,  lng: 140.75465}
+  ];
 
-  ngOnInit() {
+  constructor(private platform: Platform,public loadingCtrl: LoadingController,public toastCtrl: ToastController) { }
+
+  async ngOnInit() {
+    // Since ngOnInit() is executed before `deviceready` event,
+    // you have to wait the event.
+    await this.platform.ready();
+    await this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: this.GORYOKAKU_POINTS
+      }
+    });
+
+    let polygon: Polygon = this.map.addPolygonSync({
+      'points': this.GORYOKAKU_POINTS,
+      'strokeColor' : '#AA00FF',
+      'fillColor' : '#00FFAA',
+      'strokeWidth': 10
+    });
+
+    let points: BaseArrayClass<ILatLng> = polygon.getPoints();
+    // pontos de area
+    points.forEach((latLng: ILatLng, idx: number) => {
+      let marker: Marker = this.map.addMarkerSync({
+        draggable: true,
+        position: latLng
+      });
+      marker.on(GoogleMapsEvent.MARKER_DRAG).subscribe((params) => {
+        let position: LatLng = params[0];
+        points.setAt(idx, position);
+      });
+    });
+
+  }
+
+  async onButtonClick() {
+    this.map.clear();
+
+    this.loading = await this.loadingCtrl.create({
+      message: 'Aguarde carregando...'
+    });
+    await this.loading.present();
+
+    // Get the location of you
+    this.map.getMyLocation().then((location: MyLocation) => {
+      this.loading.dismiss();
+      console.log(JSON.stringify(location, null ,2));
+
+      // Move the map camera to the location with animation
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        tilt: 30
+      });
+
+      // add a marker
+      let marker: Marker = this.map.addMarkerSync({
+        title: 'Essa é sua localização aproximada',
+        position: location.latLng,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+
+      // show the infoWindow
+      marker.showInfoWindow();
+
+      // If clicked it, display the alert
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        this.showToast('clicked!');
+      });
+    })
+    .catch(err => {
+      this.loading.dismiss();
+      this.showToast(err.error_message);
+    });
+  }
+
+  async showToast(message: string) {
+    let toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+
+    toast.present();
   }
 
 }
