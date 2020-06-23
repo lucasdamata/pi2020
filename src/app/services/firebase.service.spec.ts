424ebd626e041639c9238aae0d4cdf087f0a375e
unknown <lucasmsilva@unipam.edu.br>
2020-06-23 20:23:13 -0300

    Tela de cadastro Firebase

--- src/app/services/firebase.service.spec.ts
+++ src/app/services/firebase.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { RegistroService } from './registro.service';
+import { FirebaseService } from './firebase.service';
 
-describe('RegistroService', () => {
+describe('FirebaseService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: RegistroService = TestBed.get(RegistroService);
+    const service: FirebaseService = TestBed.get(FirebaseService);
     expect(service).toBeTruthy();
   });
 });
